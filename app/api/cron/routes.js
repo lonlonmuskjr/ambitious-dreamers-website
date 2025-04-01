55b2a723b7a402c2cb39cd2a1970438ee9cc246c
lonlonmuskjr <43628898+lonlonmuskjr@users.noreply.github.com>
2025-04-01 03:03:45 -0400

    restructuring app router

--- app/api/cron/routes.js
+++ app/api/cron/routes.js
@@ -7,3 +7,4 @@ export async function GET() {
 if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
   return res.status(401).end('Unauthorized');
 }
+
--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold mb-4">Ambitious Dreams Enterprises</h1>
-      <p className="text-xl">Transform Your Credit, Transform Your Future</p>
-    </main>
-  );
-}
