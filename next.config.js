4153497afe9b6618ba64cb7404c2c73188814a49
lonlonmuskjr <43628898+lonlonmuskjr@users.noreply.github.com>
2025-04-01 02:25:43 -0400

    Update next.config.js
    Telling vercel where to find app folder

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // Tell Next.js where to find the App Router directory
+  appDir: "src/app",
 };
 
 module.exports = nextConfig;
