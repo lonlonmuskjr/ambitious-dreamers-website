7d9500a6c6ce033d0182b83d9f8dab70e9e064cf
lonlonmuskjr <43628898+lonlonmuskjr@users.noreply.github.com>
2025-04-01 03:19:11 -0400

    deleted wrong appdir instructions

--- next.config.js
+++ next.config.js
@@ -2,9 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // Tell Next.js where to find the App Router directory
-  appDir: "lonlonmuskjr/ambitious-dreamers-website/app",
-};
+}
 
 module.exports = nextConfig;
+
 
